10b356604199737a450b81e3d6db69ba577d54a1
Mihir Shah <shahmn96@gmail.com>
2019-09-10 15:30:54 -0400

    Update ReducedRandom.py

--- ReducedRandom.py
+++ ReducedRandom.py
@@ -8,14 +8,6 @@ import random
 import numpy
 from gym import wrappers, logger
 
-"""
-@author1 : Mihir Shah
-@Email_id : ms8830@g.rit.edu
-
-@author2: Swapnil Shah
-@Email_id: sss4174@g.rit.edu
-"""
-
 class Agent(object):
     """The world's simplest agent!"""
 
